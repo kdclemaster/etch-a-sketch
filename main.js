8bc9448da1a33d5b0b67f0f4f441bedb9c9d1d6f
kdclemaster <kdclemaster@gmail.com>
2018-08-06 20:53:22 -0400

    Basic functionality. Does not handle large values for board size well, and is minimally styled.

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function makeBoard(size = 16){
 	let board = document.querySelector('#board');
+	//cleanup
+	while(board.firstChild){
+		board.removeChild(board.firstChild);
+	}
 	//set the grid columns and rows to be equally spaced
 	board.setAttribute('style', `grid-template-columns:repeat(${size},1fr); grid-template-rows:repeat(${size},1fr)`);
 	for(i = 1; i <= size; i++){
@@ -7,8 +11,41 @@ function makeBoard(size = 16){
 			let div = document.createElement('div');
 			div.classList.add("grid-square");
 			board.appendChild(div);
+			div.addEventListener('pointerenter', hoverColor)
 		}
 	}
+	let spaces = document.querySelectorAll('div');
 }
+
+function randomColor(){
+	// generates a random rgb color string
+	let color = '';
+	let number = 0;
+	for (i = 0; i < 3; i++){
+		number = Math.random() * 255;
+		color = color + ',' + number; 
+	}
+	return color.slice(1);
+}
+
+function hoverColor(event){
+	this.setAttribute('style', `background-color: black;`);
+}
+
+function isInteger(num){
+	return (typeof num === 'number') && (num % 1 === 0);
+}
+
+function resetPrompt(){
+	let size;
+		
+	do {
+		size = parseInt(prompt("Enter the amount of squares for the width and height of the new board."));
+	} while (!(isInteger(size)))
+
+	makeBoard(size);
+}
+
+
 
 makeBoard();
